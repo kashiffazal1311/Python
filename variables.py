3eb97c48ffba24311f06df6ea292e84b1abe0618
Mohd Kashif <kashiffazal2000@gmail.com>
2023-09-19 12:14:41 +0530

    new commit

---
+++ variables.py
@@ -0,0 +1,8 @@
+# Variables and DataTypes
+
+a = 100 # its datatype is int
+b = 'Kashif' # its datatype is str
+c = "Fazal" # its datatype is str
+d = True # its datatype is bool
+
+print(type(a), type(b), type(c), (type(d)))
